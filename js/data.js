750ee87ab3b7c9785023e2e7d855a7b08efe3a7b
howard-vibely <howard@vibely.io>
2021-04-29 10:37:43 -0700

    updated project layout

--- js/data.js
+++ js/data.js
@@ -1,67 +1,74 @@
+const projectTypes = {
+  workshop: "workshop",
+  adu: "accessory dwelling unit",
+  mfh: "multi-family home",
+  ml: "campus apartment",
+};
+
 const autocadProjects = [
   {
-    type: "workshop",
+    type: projectTypes.workshop,
     title: "AutoCAD Workshop",
     path: "projects/workshops/Architecture_at_UCSD_AutoCAD_Workshop_2021",
     description: "",
   },
   {
-    type: "accessory dwelling unit",
+    type: projectTypes.adu,
     title: "ADU Plan - Original",
     path: "projects/cad/adu/ADU_plan-original",
     description: "",
   },
   {
-    type: "accessory dwelling unit",
+    type: projectTypes.adu,
     title: "ADU Plan - Proposed",
     path: "projects/cad/adu/ADU_plan-proposed",
     description: "",
   },
   {
-    type: "accessory dwelling unit",
+    type: projectTypes.adu,
     title: "ADU Plan - Kitchen V1",
     path: "projects/cad/adu/ADU_plan-kitchen_v1",
     description: "",
   },
   {
-    type: "accessory dwelling unit",
+    type: projectTypes.adu,
     title: "ADU Plan - Kitchen V2",
     path: "projects/cad/adu/ADU_plan-kitchen_v2",
     description: "",
   },
   {
-    type: "multi-family home",
+    type: projectTypes.mfh,
     title: "Multi-Family - Original Ground Floor",
     path: "projects/cad/multifamily/Multi_Family-Original_Ground_Floor",
     description: "",
   },
   {
-    type: "multi-family home",
+    type: projectTypes.mfh,
     title: "Multi-Family - Garage to Res Units",
     path:
       "projects/cad/multifamily/Multi_Family-Ground_Floor-Garage_to_Res_Units",
     description: "",
   },
   {
-    type: "multi-family home",
+    type: projectTypes.mfh,
     title: "Multi-Family - 1st Floor",
     path: "projects/cad/multifamily/Multi_Family-1st_Floor",
     description: "",
   },
   {
-    type: "multi-family home",
+    type: projectTypes.mfh,
     title: "Multi-Family - 2nd Floor",
     path: "projects/cad/multifamily/Multi_Family-2nd_Floor",
     description: "",
   },
   {
-    type: "multi-family home",
+    type: projectTypes.mfh,
     title: "Multi-Family - West North Elevation Plans",
     path: "projects/cad/multifamily/Multi_Family-West_North_Elevation_Plans",
     description: "",
   },
   {
-    type: "multi-family home",
+    type: projectTypes.mfh,
     title: "Multi-Family - East South Elevation Plans",
     path: "projects/cad/multifamily/Multi_Family-East_South_Elevation_Plans",
     description: "",
@@ -70,83 +77,83 @@ const autocadProjects = [
 
 const revitProjects = [
   {
-    type: "workshop",
+    type: projectTypes.workshop,
     title: "Revit Workshop",
     path: "projects/workshops/Architecture_at_UCSD_Revit_Workshop_2021",
     description: "",
   },
   {
-    type: "campus apartment",
+    type: projectTypes.ml,
     title: "Marshall Lowers - 1st Floor",
     path: "projects/cad/ml/ML-Building_D-1st_Floor_Plan",
     description: "",
   },
   {
-    type: "campus apartment",
+    type: projectTypes.ml,
     title: "Marshall Lowers - 2nd Floor",
     path: "projects/cad/ml/ML-Building_D-2nd_Floor_Plan",
     description: "",
   },
   {
-    type: "campus apartment",
+    type: projectTypes.ml,
     title: "Marshall Lowers - 3rd Floor",
     path: "projects/cad/ml/ML-Building_D-3rd_Floor_Plan",
     description: "",
   },
   {
-    type: "campus apartment",
+    type: projectTypes.ml,
     title: "Marshall Lowers - 4th Floor",
     path: "projects/cad/ml/ML-Building_D-4th_Floor_Plan",
     description: "",
   },
   {
-    type: "campus apartment",
+    type: projectTypes.ml,
     title: "Marshall Lowers - 3D Front Face",
     path: "projects/cad/ml/ML-Building_D-3D_Front_Face",
     description: "",
   },
   {
-    type: "campus apartment",
+    type: projectTypes.ml,
     title: "Marshall Lowers - 3D Front Left",
     path: "projects/cad/ml/ML-Building_D-3D_Front_Left",
     description: "",
   },
   {
-    type: "campus apartment",
+    type: projectTypes.ml,
     title: "Marshall Lowers - 3D Front Right",
     path: "projects/cad/ml/ML-Building_D-3D_Front_Right",
     description: "",
   },
   {
-    type: "campus apartment",
+    type: projectTypes.ml,
     title: "Marshall Lowers - 3D Left Face",
     path: "projects/cad/ml/ML-Building_D-3D_Left_Face",
     description: "",
   },
   {
-    type: "campus apartment",
+    type: projectTypes.ml,
     title: "Marshall Lowers - 3D Right Face",
     path: "projects/cad/ml/ML-Building_D-3D_Right_Face",
     description: "",
   },
   {
-    type: "campus apartment",
+    type: projectTypes.ml,
     title: "Marshall Lowers - 3D Back Face",
     path: "projects/cad/ml/ML-Building_D-3D_Back_Face",
     description: "",
   },
   {
-    type: "campus apartment",
+    type: projectTypes.ml,
     title: "Marshall Lowers - 3D Back Left",
     path: "projects/cad/ml/ML-Building_D-3D_Back_Left",
     description: "",
   },
   {
-    type: "campus apartment",
+    type: projectTypes.ml,
     title: "Marshall Lowers - 3D Back Right",
     path: "projects/cad/ml/ML-Building_D-3D_Back_Right",
     description: "",
   },
 ];
 
-export { autocadProjects, revitProjects };
+export { autocadProjects, revitProjects, projectTypes };
--- js/projects.js
+++ js/projects.js
@@ -1,9 +1,7 @@
-import { autocadProjects, revitProjects } from "./data.js";
+import { autocadProjects, revitProjects, projectTypes } from "./data.js";
 
-const autocadSection = document.getElementById("autocad-section");
-autocadProjects.forEach((project) => {
-  autocadSection.innerHTML += `
-    <div class="col-lg-4 col-md-6 grid-item mb-4">
+const getProjectItem = (project) => `
+    <div class="col-lg-4 col-md-6 grid-item">
         <div class="listing-item">
             <div class="position-relative">
             <ul class="list-inline listing-tags m-0">
@@ -26,34 +24,54 @@ autocadProjects.forEach((project) => {
             </div>
         </div>
     </div>
-  `;
+`;
+
+const autocadSection1 = document.getElementById("autocad-section-1");
+const autocadSection2 = document.getElementById("autocad-section-2");
+const autocadSection3 = document.getElementById("autocad-section-3");
+autocadProjects.forEach((project) => {
+  const projectItem = getProjectItem(project);
+  switch (project.type) {
+    case projectTypes.workshop:
+      autocadSection1.innerHTML += projectItem;
+      break;
+    case projectTypes.adu:
+      autocadSection2.innerHTML += projectItem;
+      break;
+    case projectTypes.mfh:
+      autocadSection3.innerHTML += projectItem;
+      break;
+    default:
+      break;
+  }
 });
 
-const revitSection = document.getElementById("revit-section");
+const revitSection1 = document.getElementById("revit-section-1");
+const revitSection2 = document.getElementById("revit-section-2");
+const revitSection3 = document.getElementById("revit-section-3");
+const revitSection4 = document.getElementById("revit-section-4");
+const revitSection5 = document.getElementById("revit-section-5");
 revitProjects.forEach((project) => {
-  revitSection.innerHTML += `
-    <div class="col-lg-4 col-md-6 grid-item mb-4">
-        <div class="listing-item">
-            <div class="position-relative">
-            <ul class="list-inline listing-tags m-0">
-                <li class="list-inline-item"><a class="reset-anchor font-weight-normal text-gray text-small"
-                    href="${project.path}.pdf">${project.type}</a></li>
-            </ul><a class="reset-anchor d-block listing-img-holder" href="${project.path}.pdf"><img
-                class="img-fluid" src="${project.path}.PNG" alt="${project.title}" height="400">
-                <p class="mb-0 text-primary small d-flex align-items-center listing-btn"> <span>Take a closer
-                    look</span>
-                <svg class="svg-icon text-primary svg-icon-sm ml-2">
-                    <use xlink:href="#arrow-right-1"> </use>
-                </svg>
-                </p>
-            </a>
-            </div>
-            <div class="py-3"><a class="reset-anchor" href="${project.path}.pdf">
-                <h2 class="h5 listing-item-heading">${project.title}</h2>
-            </a>
-            <p class="text-small mb-0 listing-item-description">${project.description}</p>
-            </div>
-        </div>
-    </div>
-  `;
+  const projectItem = getProjectItem(project);
+  const title = project.title.toLowerCase();
+  switch (project.type) {
+    case projectTypes.workshop:
+      revitSection1.innerHTML += projectItem;
+      break;
+    case projectTypes.ml:
+      if (
+        title.includes("floor") ||
+        title.includes("front face") ||
+        title.includes("back face")
+      )
+        revitSection2.innerHTML += projectItem;
+      else if (title.includes("left face") || title.includes("right face"))
+        revitSection3.innerHTML += projectItem;
+      else if (title.includes("front left") || title.includes("front right"))
+        revitSection4.innerHTML += projectItem;
+      else revitSection5.innerHTML += projectItem;
+      break;
+    default:
+      break;
+  }
 });
