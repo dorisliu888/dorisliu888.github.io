3fa63b9ccac54d3e80e8aba0ceb1cfad3381cd62
howard-vibely <howard@vibely.io>
2021-06-08 10:56:57 -0700

    fix render ml

--- js/projects.js
+++ js/projects.js
@@ -6,9 +6,15 @@ const getProjectItem = (project) => `
             <div class="position-relative">
             <ul class="list-inline listing-tags m-0">
                 <li class="list-inline-item"><a class="reset-anchor font-weight-normal text-gray text-small"
-                    href="${project.link ? project.link : (project.path + '.pdf')}">${project.type}</a></li>
-            </ul><a class="reset-anchor d-block listing-img-holder" href="${project.link ? project.link : (project.path + '.pdf')}"><img
-                class="img-fluid" src="${project.path}.PNG" alt="${project.title}" height="400">
+                    href="${
+                      project.link ? project.link : project.path + ".pdf"
+                    }">${project.type}</a></li>
+            </ul><a class="reset-anchor d-block listing-img-holder" href="${
+              project.link ? project.link : project.path + ".pdf"
+            }"><img
+                class="img-fluid" src="${project.path}.PNG" alt="${
+  project.title
+}" height="400">
                 <p class="mb-0 text-primary small d-flex align-items-center listing-btn"> <span>Take a closer
                     look</span>
                 <svg class="svg-icon text-primary svg-icon-sm ml-2">
@@ -17,10 +23,14 @@ const getProjectItem = (project) => `
                 </p>
             </a>
             </div>
-            <div class="py-3"><a class="reset-anchor" href="${project.link ? project.link : (project.path + '.pdf')}">
+            <div class="py-3"><a class="reset-anchor" href="${
+              project.link ? project.link : project.path + ".pdf"
+            }">
                 <h2 class="h5 listing-item-heading">${project.title}</h2>
             </a>
-            <p class="text-small mb-0 listing-item-description">${project.description}</p>
+            <p class="text-small mb-0 listing-item-description">${
+              project.description
+            }</p>
             </div>
         </div>
     </div>
@@ -34,6 +44,9 @@ const revitSection1 = document.getElementById("revit-section-1");
 revitProjects.forEach((project) => {
   const projectItem = getProjectItem(project);
   switch (project.type) {
+    case projectTypes.ml:
+      revitSection1.innerHTML += projectItem;
+      break;
     case projectTypes.workshop:
       revitSection1.innerHTML += projectItem;
       break;
